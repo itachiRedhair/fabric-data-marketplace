009e490b7c7479df7eb78e4279aa441eab7b0953
Akshay Milmile <akshay.milmile@gmail.com>
2019-02-12 09:02:49 +0530

    forgot to commit, so autmoatically comitting...

--- scripts/generateConfigTX.sh
+++ scripts/generateConfigTX.sh
@@ -28,28 +28,28 @@ echo
 echo "#################################################################"
 echo "####### Generating anchor peer update for MPBroker ##########"
 echo "#################################################################"
-$TOOLS/configtxgen -profile FiveOrgsChannel -outputAnchorPeersUpdate $ANCHOR_TRANSACTIONS_PATH/MPBrokerAnchors.tx -channelID $CHANNEL_NAME -asOrg MPBroker
+$TOOLS/configtxgen -profile FiveOrgsChannel -outputAnchorPeersUpdate $ANCHOR_TRANSACTIONS_PATH/MPBrokerAnchors.tx -channelID $CHANNEL_NAME -asOrg MPBrokerOrg
 
 echo
 echo "#################################################################"
 echo "####### Generating anchor peer update for CustomerA ##########"
 echo "#################################################################"
-$TOOLS/configtxgen -profile FiveOrgsChannel -outputAnchorPeersUpdate $ANCHOR_TRANSACTIONS_PATH/CustomerAAnchors.tx -channelID $CHANNEL_NAME -asOrg CustomerA
+$TOOLS/configtxgen -profile FiveOrgsChannel -outputAnchorPeersUpdate $ANCHOR_TRANSACTIONS_PATH/CustomerAAnchors.tx -channelID $CHANNEL_NAME -asOrg CustomerAOrg
 
 echo
 echo "#################################################################"
 echo "#######    Generating anchor peer update for CustomerB   ##########"
 echo "#################################################################"
-$TOOLS/configtxgen -profile FiveOrgsChannel -outputAnchorPeersUpdate $ANCHOR_TRANSACTIONS_PATH/CustomerBAnchors.tx -channelID $CHANNEL_NAME -asOrg CustomerB
+$TOOLS/configtxgen -profile FiveOrgsChannel -outputAnchorPeersUpdate $ANCHOR_TRANSACTIONS_PATH/CustomerBAnchors.tx -channelID $CHANNEL_NAME -asOrg CustomerBOrg
 
 echo
 echo "##################################################################"
 echo "####### Generating anchor peer update for CustomerC ##########"
 echo "##################################################################"
-$TOOLS/configtxgen -profile FiveOrgsChannel -outputAnchorPeersUpdate $ANCHOR_TRANSACTIONS_PATH/CustomerCAnchors.tx -channelID $CHANNEL_NAME -asOrg CustomerC
+$TOOLS/configtxgen -profile FiveOrgsChannel -outputAnchorPeersUpdate $ANCHOR_TRANSACTIONS_PATH/CustomerCAnchors.tx -channelID $CHANNEL_NAME -asOrg CustomerCOrg
 
 echo
 echo "##################################################################"
 echo "#######   Generating anchor peer update for CustomerD   ##########"
 echo "##################################################################"
-$TOOLS/configtxgen -profile FiveOrgsChannel -outputAnchorPeersUpdate $ANCHOR_TRANSACTIONS_PATH/CustomerDAnchors.tx -channelID $CHANNEL_NAME -asOrg CustomerD
+$TOOLS/configtxgen -profile FiveOrgsChannel -outputAnchorPeersUpdate $ANCHOR_TRANSACTIONS_PATH/CustomerDAnchors.tx -channelID $CHANNEL_NAME -asOrg CustomerDOrg
